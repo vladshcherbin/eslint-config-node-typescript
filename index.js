40f13f8fcc02b5a2ad4dd6da9111b15cff768d12
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-10-07 15:52:54 +0300

    Update no-restricted-syntax rule

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ module.exports = {
     '@typescript-eslint/semi': ['error', 'never'],
     'import/order': ['error', { alphabetize: { order: 'asc' } }],
     'no-await-in-loop': 'off',
+    'no-restricted-syntax': [
+      'error',
+      {
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+        selector: 'ForInStatement'
+      },
+      {
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+        selector: 'LabeledStatement'
+      },
+      {
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+        selector: 'WithStatement'
+      }
+    ],
     'object-curly-newline': ['error', { consistent: true, multiline: true }],
     'sort-destructure-keys/sort-destructure-keys': 'error',
     'sort-imports': ['error', { ignoreDeclarationSort: true }],
